de2c3d07093403fb86801767efe1155f99764e3c
fwcd <fwcdmail@gmail.com>
2023-10-19 13:28:43 +0100

    [qt5-base] Downgrade to Qt 5.12.12 for testing

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -46,38 +46,18 @@ endif()
 
 qt_download_submodule(  OUT_SOURCE_PATH SOURCE_PATH
                         PATCHES
-                            # CVE fixes from https://download.qt.io/official_releases/qt/5.15/
-                            patches/CVE-2023-24607-qtbase-5.15.diff
-                            patches/CVE-2023-32762-qtbase-5.15.diff
-                            patches/CVE-2023-32763-qtbase-5.15.diff
-                            patches/CVE-2023-33285-qtbase-5.15.diff
-                            patches/CVE-2023-34410-qtbase-5.15.diff
-                            patches/CVE-2023-37369-qtbase-5.15.diff
-                            patches/CVE-2023-38197-qtbase-5.15.diff
-                            patches/CVE-2023-43114-5.15.patch
-
-                            patches/winmain_pro.patch          #Moves qtmain to manual-link
-                            patches/windows_prf.patch          #fixes the qtmain dependency due to the above move
-                            patches/qt_app.patch               #Moves the target location of qt5 host apps to always install into the host dir.
-                            patches/gui_configure.patch        #Patches the gui configure.json to break freetype/fontconfig autodetection because it does not include its dependencies.
-                            patches/xlib.patch                 #Patches Xlib check to actually use Pkgconfig instead of makeSpec only
-                            patches/egl.patch                  #Fix egl detection logic.
-                            patches/qtbug_96392.patch          #Backport fix for QTBUG-96392
-                            patches/mysql_plugin_include.patch #Fix include path of mysql plugin
-                            patches/mysql-configure.patch      #Fix mysql project
-                            patches/cocoa.patch                #Fix missing include on macOS Monterrey, https://code.qt.io/cgit/qt/qtbase.git/commit/src/plugins/platforms/cocoa?id=dece6f5840463ae2ddf927d65eb1b3680e34a547
-                            patches/xcode-15.patch             #From https://codereview.qt-project.org/c/qt/qtbase/+/503172
-                            patches/patch-qtbase-memory_resource.diff # From https://bugreports.qt.io/browse/QTBUG-114316
-                            #patches/static_opengl.patch       #Use this patch if you really want to statically link angle on windows (e.g. using -opengl es2 and -static).
-                                                               #Be carefull since it requires definining _GDI32_ for all dependent projects due to redefinition errors in the
-                                                               #the windows supplied gl.h header and the angle gl.h otherwise.
-                            # CMake fixes
-                            patches/Qt5BasicConfig.patch
-                            patches/Qt5PluginTarget.patch
-                            patches/create_cmake.patch
-                            patches/Qt5GuiConfigExtras.patch   # Patches the library search behavior for EGL since angle is not build with Qt
-                            patches/fix_angle.patch            # Failed to create OpenGL context for format QSurfaceFormat ...
-                            patches/mingw9.patch               # Fix compile with MinGW-W64 9.0.0: Redefinition of 'struct _FILE_ID_INFO'
+                            patches/winmain_pro.patch   #Moves qtmain to manual-link
+                            patches/windows_prf.patch   #fixes the qtmain dependency due to the above move
+                            patches/qt_app.patch        #Moves the target location of qt5 host apps to always install into the host dir. 
+                            patches/gui_configure.patch #Patches the gui configure.json to break freetype autodetection because it does not include its dependencies.
+                            #patches/static_opengl.patch #Use this patch if you really want to statically link angle on windows (e.g. using -opengl es2 and -static). 
+                                                         #Be carefull since it requires definining _GDI32_ for all dependent projects due to redefinition errors in the 
+                                                         #the windows supplied gl.h header and the angle gl.h otherwise. 
+                            patches/nsimage_qimage.patch  # add [NSImage imageFromQImage:] (required for the QCocoaDrag patch)
+                            patches/QCocoaDrag-avoid-using-the-deprecated-API-if-possibl.patch # allow Apple SDK >= 10.14
+                            patches/arm64_qiosurfacegraphicsbuffer.patch   # allow to build on arm64
+                            patches/scrollbars_style.patch # Never handle scrollbars styled with box or border changes as transient
+                            patches/qmake_xcode15.patch # Fix Xcode 15 (macOS 14 Sonoma) build, see https://github.com/Homebrew/homebrew-core/pull/145729
                     )
 
 # Remove vendored dependencies to ensure they are not picked up by the build
