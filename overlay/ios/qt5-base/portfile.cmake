113770c6f82ff8c7374bb288edfd8d976224c858
fwcd <fwcd@fwcd.dev>
2024-02-09 19:31:16 +0100

    [qt5-base] Add iOS IOKit patch

--- overlay/ios/qt5-base/portfile.cmake
+++ overlay/ios/qt5-base/portfile.cmake
@@ -40,6 +40,7 @@ qt_download_submodule(  OUT_SOURCE_PATH SOURCE_PATH
                             patches/replace_result_of.patch # Replace usage of std::result_of with decltype
                             patches/scrollbars_style.patch # Never handle scrollbars styled with box or border changes as transient
                             patches/qmake_xcode15.patch # Fix Xcode 15 (macOS 14 Sonoma) build, see https://github.com/Homebrew/homebrew-core/pull/145729
+                            patches/ios_iokit_qglobal.patch # Exclude IOKit-specifics from QGlobal on iOS (IOKit is iOS 16+ and this older Qt likely assumed it was still macOS-only)
                     )
 
 # Remove vendored dependencies to ensure they are not picked up by the build
