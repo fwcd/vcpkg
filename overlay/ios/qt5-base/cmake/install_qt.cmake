9d13472e77289cea9fa44156b56b5979e2251202
fwcd <fwcd@fwcd.dev>
2024-02-09 23:57:07 +0100

    [qt5-base] Skip clean step again when targeting iOS

--- overlay/ios/qt5-base/cmake/install_qt.cmake
+++ overlay/ios/qt5-base/cmake/install_qt.cmake
@@ -94,11 +94,11 @@ function(install_qt)
             LOGNAME build-${_build_triplet}
         )
         
-        if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
+        if(VCPKG_TARGET_IS_OSX)
            # For some reason there will be an error on MacOSX without this clean!
             message(STATUS "Cleaning after build before install ${_build_triplet}")
             vcpkg_execute_required_process(
-                COMMAND ${INVOKE_SINGLE} clean DESTINATION='id=0'
+                COMMAND ${INVOKE_SINGLE} clean
                 WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${_build_triplet}
                 LOGNAME cleaning-2-${_build_triplet}
             )
@@ -106,6 +106,7 @@ function(install_qt)
         
         message(STATUS "Fixing makefile installation path ${_build_triplet}")
         qt_fix_makefile_install("${CURRENT_BUILDTREES_DIR}/${_build_triplet}")
+
         message(STATUS "Installing ${_build_triplet}")
         vcpkg_execute_required_process(
             COMMAND ${INVOKE} install
