94780809dae609acd523e94581c553afdafe5b0b
fwcd <fwcd@fwcd.dev>
2024-02-29 04:08:41 +0100

    [portmidi] Use updated iOS patches based on latest master
    The patches are based on https://github.com/PortMidi/portmidi/pull/65

--- overlay/ios/portmidi/portfile.cmake
+++ overlay/ios/portmidi/portfile.cmake
@@ -1,23 +1,31 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO mixxxdj/portmidi
-    REF 236
-    SHA512 901729440c4b8c654ab17c4c4f1e3986813bf1e4ad1a874f46e7b1ee0c9ef4ee9f1ecfdf71012fb56b055a6185194dd22c520ae87b9f2259c18af5d189ca57da
-    HEAD_REF main
+    REPO PortMidi/portmidi
+    REF "928520f7b79f371854387cb480b4e3b5bf5dac95"
+    SHA512 e5ba3f794d8c9b86099d7ec23eb662202a2c8a405e01e06b6dca6eda9d10c04d5ad94b6998b0ece428ed34837b72695d459dfb0a9d81ce178422398f38860933
+    HEAD_REF master
     PATCHES
-        "remove-legacy-carbon-apis.patch"
-        "pt-remove-coreaudio-hosttime-dependency.patch"
-        "pt-use-standard-uint64.patch"
-        "pm-remove-coreaudio-hosttime-dependency.patch"
-        "pm-use-corefoundation-endian-utils.patch"
+        "ios-support.patch" # Upstream PR: https://github.com/PortMidi/portmidi/pull/65
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
-vcpkg_cmake_install()
+if(VCPKG_CRT_LINKAGE STREQUAL static)
+    set(PM_USE_STATIC_RUNTIME ON)
+else()
+    set(PM_USE_STATIC_RUNTIME OFF)
+endif()
 
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DPM_USE_STATIC_RUNTIME="${PM_USE_STATIC_RUNTIME}"
+)
+
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/PortMidi)
 
-file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/license.txt")
