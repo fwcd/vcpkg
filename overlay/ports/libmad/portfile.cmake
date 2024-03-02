48b0fabee986387de94c3a6cf82c9553d7af5e40
fwcd <fwcd@fwcd.dev>
2024-03-02 04:42:24 +0100

    [libmad] Avoid architecture-specific assembly when targeting WASM

--- overlay/ports/libmad/portfile.cmake
+++ overlay/ports/libmad/portfile.cmake
@@ -16,10 +16,19 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         aso ASO
 )
 
+set(EXTRA_OPTIONS)
+
+# Avoid architecture-specific assembly when targeting WASM. For a full list of flags, see
+# https://codeberg.org/tenacityteam/libmad/src/commit/84ba587793d61caadf6d1f6c0d94c3e165874a50/CMakeLists.txt
+if(VCPKG_TARGET_IS_EMSCRIPTEN)
+    list(APPEND EXTRA_OPTIONS "-DFPM_64BIT=OFF -DFPM_INTEL=OFF -DFPM_ARM=OFF -DFPM_MIPS=OFF -DFPM_SPARC=OFF -DFPM_PCC=OFF -DFPM_DEFAULT=ON")
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        ${EXTRA_OPTIONS}
         -DEXAMPLE=OFF
 )
 
