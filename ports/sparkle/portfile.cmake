8e8980cc5b254bc7c2f1c7be065cc322c4be3ac5
fwcd <fwcd@fwcd.dev>
2024-04-09 22:03:42 +0200

    [sparkle] Respect VCPKG_TARGET_ARCHITECTURE and VCPKG_BUILD_TYPE

--- ports/sparkle/portfile.cmake
+++ ports/sparkle/portfile.cmake
@@ -6,6 +6,14 @@ vcpkg_from_github(
     HEAD_REF 2.x
 )
 
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(_xcode_arch "x86_64")
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    set(_xcode_arch "arm64")
+else()
+    message(FATAL_ERROR "Unsupported architecture ${VCPKG_TARGET_ARCHITECTURE}!" )
+endif()
+
 foreach(_buildtype IN ITEMS "debug" "release")
     if(NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "${_buildtype}")
         if("${_buildtype}" STREQUAL "debug")
@@ -24,19 +32,20 @@ foreach(_buildtype IN ITEMS "debug" "release")
         vcpkg_execute_build_process(
             COMMAND xcodebuild
                 -scheme Distribution
+                -arch ${_xcode_arch}
                 -configuration "${_xcode_configuration}"
                 -derivedDataPath "${_builddir}"
                 build
             WORKING_DIRECTORY "${SOURCE_PATH}"
             LOGNAME "build-${TARGET_TRIPLET}-${_short_build_type}"
         )
-    endif()
 
-    message(STATUS "Installing ${TARGET_TRIPLET}-${_short_build_type}")
-    file(
-        COPY "${_builddir}/Build/Products/${_xcode_configuration}/Sparkle.framework"
-        DESTINATION "${_install_destdir}/lib"
-    )
+        message(STATUS "Installing ${TARGET_TRIPLET}-${_short_build_type}")
+        file(
+            COPY "${_builddir}/Build/Products/${_xcode_configuration}/Sparkle.framework"
+            DESTINATION "${_install_destdir}/lib"
+        )
+    endif()
 endforeach()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
